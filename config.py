212ddca9b8b04f4bbfa7406a94b6cdabd67ce01f
Solar <solarrender5@gmail.com>
2026-06-18 11:14:04 +0530

    link issue fixed

--- config.py
+++ config.py
@@ -9,5 +9,5 @@ class Config:
     SUPERADMIN_USER   = os.environ.get('SUPERADMIN_USER', 'superadmin')
     SUPERADMIN_PASS   = os.environ.get('SUPERADMIN_PASS', 'changeme')
     FAST2SMS_KEY      = os.environ.get('FAST2SMS_KEY', '')
-    BASE_URL          = os.environ.get('BASE_URL', 'http://localhost:5000')
+    BASE_URL          = os.environ.get('BASE_URL', 'http://localhost:5000').rstrip('/')
     MAX_CONTENT_LENGTH = 50 * 1024 * 1024
